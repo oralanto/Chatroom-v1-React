d9ea0412bb5da999663f8d79fcbc160d69ac5a2e
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-02-08 20:38:54 +0100

    Composant Form Messages et Message + style basique

---
+++ src/components/Form/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.scss';
+
+const Form = () => (
+  <form className="form">
+    <input type="text" placeholder="Saisissez votre message..." />
+    <button type="submit">></button>
+  </form>
+);
+
+export default Form;
---
+++ src/components/Messages/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './style.scss';
+
+const Messages = () => (
+  <>
+    <li className="messages__user">Super Chat</li>
+    <li className="messages__message">Coucou toi</li>
+  </>
+);
+
+export default Messages;
